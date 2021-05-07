60c95aa2a91d738fcbd9102b4484f766b071b836
Palistha Shakya <caroze20@gmail.com>
2021-05-07 14:59:49 +0545

    add sh file

---
+++ qualiti-script.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+ 
+  set -ex
+ 
+  PROJECT_ID='332'
+  API_KEY='YlV1hYJKHG8Hx0eenTN03alZSm9mxexw81UygeZs'
+  CLIENT_ID='fb52d1fee269eca2ef6d222cc56bc613'
+  SCOPES=['"ViewTestResults"','"ViewAutomationHistory"']
+  API_URL='https://7iggpnqgq9.execute-api.us-east-2.amazonaws.com/udbodh/api'
+  INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjM3MzAsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiY2lyY2xlY2kiLCJvcHRpb25zIjp7fSwiaWF0IjoxNjE5MTY1NjYzfQ.wAyk3Khtfigmm_f_nF7CeIwKW7mS1_XEYxrdn7NuHns'
+  INTEGRATIONS_API_URL='<replace-with-ngrok-url>'
+ 
+  apt-get update -y
+  apt-get install -y jq
+ 
+  #Trigger test run
+  TEST_RUN_ID="$( \
+    curl -X POST -G ${INTEGRATIONS_API_URL}/api/integrations/circleci/${PROJECT_ID}/events \
+      -d 'token='$INTEGRATION_JWT_TOKEN''\
+      -d 'triggerType=Deploy'\
+    | jq -r '.test_run_id')"
+ 
+  AUTHORIZATION_TOKEN="$( \
+    curl -X POST -G ${API_URL}/auth/token \
+    -H 'x-api-key: '${API_KEY}'' \
+    -H 'client_id: '${CLIENT_ID}'' \
+    -H 'scopes: '${SCOPES}'' \
+    | jq -r '.token')"
+ 
+  # Wait until the test run has finished
+  TOTAL_ITERATION=200
+  I=1
+  while : ; do
+     RESULT="$( \
+     curl -X GET ${API_URL}/automation-history?project_id=${PROJECT_ID}\&test_run_id=${TEST_RUN_ID} \
+     -H 'token: Bearer '$AUTHORIZATION_TOKEN'' \
+     -H 'x-api-key: '${API_KEY}'' \
+    | jq -r '.[0].finished')"
+    if [ "$RESULT" != null ]; then
+      break;
+    if [ "$I" -ge "$TOTAL_ITERATION" ]; then
+      echo "Exit qualiti execution for taking too long time.";
+      exit 1;
+    fi
+    fi
+      sleep 15;
+  done
+ 
+  # # Once finished, verify the test result is created and that its passed
+  TEST_RUN_RESULT="$( \
+    curl -X GET ${API_URL}/test-results?test_run_id=${TEST_RUN_ID}\&project_id=${PROJECT_ID} \
+      -H 'token: Bearer '$AUTHORIZATION_TOKEN'' \
+      -H 'x-api-key: '${API_KEY}'' \
+    | jq -r '.[0].status' \
+  )"
+  echo "Qualiti E2E Tests ${TEST_RUN_RESULT}"
+  if [ "$TEST_RUN_RESULT" = "Passed" ]; then
+    exit 0;
+  fi
+  exit 1;
