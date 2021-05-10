b204c8a615cd5a0e24fca0424d11bda7c7f9144f
Palistha Shakya <caroze20@gmail.com>
2021-05-10 12:43:38 +0545

    edit

--- qualiti-script.sh
+++ qualiti-script.sh
@@ -8,7 +8,7 @@
   SCOPES=['"ViewTestResults"','"ViewAutomationHistory"']
   API_URL='https://7iggpnqgq9.execute-api.us-east-2.amazonaws.com/udbodh/api'
   INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjM3MzAsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiY2lyY2xlY2kiLCJvcHRpb25zIjp7fSwiaWF0IjoxNjE5MTY1NjYzfQ.wAyk3Khtfigmm_f_nF7CeIwKW7mS1_XEYxrdn7NuHns'
-  INTEGRATIONS_API_URL='http://02a387e041ca.ngrok.io'
+  INTEGRATIONS_API_URL='http://071f10414e7c.ngrok.io'
  
   sudo apt-get update -y
   sudo apt-get install -y jq
